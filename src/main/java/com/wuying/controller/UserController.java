4a4f794079499f76e6da55e605eeb1403d0a90a1
liujunwuying <liujun_wuying@163.com>
2021-09-28 16:21:49 +0800

    feat:init

--- src/main/java/com/wuying/controller/UserController.java
+++ src/main/java/com/wuying/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.wuying.controller;
 
+import com.wuying.cloud.context.holder.ApplicationContextHolder;
 import com.wuying.entity.User;
 import com.wuying.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 /**
- *
  * @author wuying
  * @since 1.0.0
  * @date 2021-09-24
